ee3c61c05a16ee0b2fcf0c87592a3b4f41ca4b41
ypandey9 <128962086+ypandey9@users.noreply.github.com>
2023-09-06 07:14:29 +0530

    Add files via upload

---
+++ ZZMvcApp/src/main/java/com/gl/demo/DateServlet.java
@@ -0,0 +1,23 @@
+package com.gl.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DateServlet
+ */
+public class DateServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<h1 style='color:green'>Today's date on server: "+new Date()+"</h1>");
+	}
+
+	}
